7490505a657b6eeaaf2c19e6104cb9194f199548
Faey - Dev <agustinovfreeze@gmail.com>
2026-06-06 02:43:44 +0700

    Add files via upload

---
+++ config/app.php
@@ -0,0 +1,9 @@
+<?php
+// config/app.php
+
+return [
+    'name' => 'SDAVerse Beauty ERP',
+    'url' => 'http://localhost/skincare-inventory',
+    'timezone' => 'Asia/Jakarta',
+    'simulated_date' => '2026-06-05' // Tanggal simulasi sistem untuk validasi Near-Expiry (FEFO)
+];
---
+++ config/database.php
@@ -0,0 +1,11 @@
+<?php
+// config/database.php
+
+return [
+    'host' => 'localhost',
+    'port' => '3306',
+    'dbname' => 'skincare_inventory', // Sesuaikan dengan nama database di phpMyAdmin Anda
+    'username' => 'root',             // Default XAMPP username
+    'password' => '',                 // Default XAMPP password (kosong)
+    'charset' => 'utf8mb4'
+];
